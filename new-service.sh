9d0f32f2755dc1242fd9f5c383f69bb71e2e901d
Usama Ahsan <usama8800@gmail.com>
2021-05-16 07:38:50 +0500

    .

--- new-service.sh
+++ new-service.sh
@@ -8,15 +8,13 @@ fi
 upper=$(echo "$1" | sed -e 's/\(.*\)/\U\1/')
 lower=$(echo "$1" | sed -e 's/\(.*\)/\L\1/')
 
-cp sample.config.json "$lower.config.json"
-sed -i -e "s/SAMPLE/$upper/g" "$lower.config.json"
+# cp sample.config.json "$lower.config.json"
+# sed -i -e "s/SAMPLE/$upper/g" "$lower.config.json"
 
 if grep {} restart-all.sh 1>/dev/null; then
     sed -i -e "s/systemctl restart {}/systemctl restart {$lower}/" restart-all.sh
 else
     sed -i -e "s/systemctl restart {\([^}]\)/systemctl restart {$lower,\1/" restart-all.sh
-    else
-    sed -i -e "s/systemctl restart {}/systemctl restart {$lower}/" restart-all.sh
 fi
 
 path=$(pwd)
