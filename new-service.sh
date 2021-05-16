1b3199ac72ffb7b1720e086303014738fab37bf5
Usama Ahsan <usama8800@gmail.com>
2021-05-16 06:22:31 +0500

    .

--- new-service.sh
+++ new-service.sh
@@ -11,14 +11,14 @@ lower=$(echo "$1" | sed -e 's/\(.*\)/\L\1/')
 cp sample.config.json "$lower.config.json"
 sed -i -e "s/SAMPLE/$upper/g" "$lower.config.json"
 
-grep , restart-all.sh
+grep , restart-all.sh 1>/dev/null
 if [ "$?" -eq 0 ]; then
     sed -i -e "s/systemctl restart {\([^}]\)/systemctl restart {$lower,\1/" restart-all.sh
 else
     sed -i -e "s/systemctl restart {}/systemctl restart {$lower}/" restart-all.sh
 fi
 
-$path=$(pwd)
+path=$(pwd)
 cd /etc/systemd/system
 sudo cp "$path/sample.service" "$lower.service"
 sudo sed -i -e "s/Sample/$upper/g" -e "s/sample/$lower/g" "$lower.service"
