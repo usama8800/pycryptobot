61a5a6fedf667169dfa9c92e7123932f914f9101
Usama Ahsan <usama8800@gmail.com>
2021-08-18 04:52:33 +0500

    .

--- config.py
+++ config.py
@@ -14,10 +14,9 @@ class Config:
             config = {}
         self.binance_key: str = config["binance_key"]
         self.binance_secret: str = config["binance_secret"]
-        self.telegram_token: str = config["telegram_token"]
-        self.telegram_client_id: str = config["telegram_client_id"]
         self.tc_key: str = config["3commas_key"]
         self.tc_secret: str = config["3commas_secret"]
         self.simstartdate: str = config["simstartdate"]
         self.simenddate: str = config["simenddate"]
         self.granularity: str = config["granularity"]
+        self.discordHook: str = config["discordHook"]
